e430cb96a9c663521444904346e6913a53f70827
kraqinc <kraqai.c@gmail.com>
2026-05-06 19:20:40 -0500

    Add/update app/src/main/java/com/profeloop/kalanba/subjects/SubjectListFragment.kt

--- app/src/main/java/com/profeloop/kalanba/subjects/SubjectListFragment.kt
+++ app/src/main/java/com/profeloop/kalanba/subjects/SubjectListFragment.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.tabs.TabLayout
-import com.profeloop.kalanba.MainActivity
-import com.profeloop.kalanba.R
 import com.profeloop.kalanba.databinding.FragmentSubjectListBinding
-import com.profeloop.kalanba.models.Subject
 import com.profeloop.kalanba.utils.Constants
 import com.profeloop.kalanba.utils.FirebaseUtils
 import kotlinx.coroutines.launch
@@ -21,12 +19,14 @@ class SubjectListFragment : Fragment() {
 
     private var _binding: FragmentSubjectListBinding? = null
     private val binding get() = _binding!!
+    private val args: SubjectListFragmentArgs by navArgs()
+
+    private var currentPeriodo = 1
     private lateinit var adapter: SubjectAdapter
-    private var grado: Int = 8
-    private var selectedPeriodo: Int = 1
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSubjectListBinding.inflate(inflater, container, false)
@@ -36,65 +36,41 @@ class SubjectListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        grado = arguments?.getInt(Constants.EXTRA_GRADO) ?: 8
+        val grado = args.extraGrado
 
-        (activity as? MainActivity)?.supportActionBar?.title = "Grado $grado°"
-
-        setupPeriodoTabs()
-        setupAdapter()
-        loadSubjects()
-    }
-
-    private fun setupPeriodoTabs() {
-        Constants.PERIODOS.forEach { periodo ->
-            binding.tabsPeriodo.addTab(
-                binding.tabsPeriodo.newTab().setText("Período $periodo")
-            )
+        // Setup tabs
+        for (i in 1..4) {
+            binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Período $i"))
         }
-        binding.tabsPeriodo.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+        adapter = SubjectAdapter(emptyList()) { subject ->
+            val action = SubjectListFragmentDirections
+                .actionSubjectListToTaskList(grado, subject, currentPeriodo)
+            findNavController().navigate(action)
+        }
+
+        binding.rvSubjects.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvSubjects.adapter = adapter
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab) {
-                selectedPeriodo = tab.position + 1
-                loadSubjects()
+                currentPeriodo = tab.position + 1
+                loadSubjects(grado)
             }
             override fun onTabUnselected(tab: TabLayout.Tab) {}
             override fun onTabReselected(tab: TabLayout.Tab) {}
         })
+
+        loadSubjects(grado)
     }
 
-    private fun setupAdapter() {
-        adapter = SubjectAdapter(
-            grado          = grado,
-            currentUser    = (activity as? MainActivity)?.currentUser,
-            onSubjectClick = { asignatura ->
-                findNavController().navigate(
-                    R.id.action_subjectListFragment_to_taskListFragment,
-                    bundleOf(
-                        Constants.EXTRA_GRADO      to grado,
-                        Constants.EXTRA_ASIGNATURA to asignatura,
-                        Constants.EXTRA_PERIODO    to selectedPeriodo
-                    )
-                )
+    private fun loadSubjects(grado: Int) {
+        lifecycleScope.launch {
+            val subjectsWithTeachers = Constants.SUBJECTS.map { subject ->
+                val teacher = FirebaseUtils.checkTeacherInSubject(grado, subject)
+                Pair(subject, teacher)
             }
-        )
-        binding.rvSubjects.adapter = adapter
-    }
-
-    private fun loadSubjects() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            binding.progressBar.visibility = View.VISIBLE
-
-            val subjectItems = Subject.TODAS_LAS_ASIGNATURAS.map { nombre ->
-                val teacher = FirebaseUtils.getSubjectTeacher(grado, nombre)
-                SubjectItem(
-                    nombre          = nombre,
-                    profesorNombre  = teacher?.nombreCompleto() ?: "Sin asignar",
-                    tieneProfesor   = teacher != null,
-                    emoji           = Subject.emojiParaAsignatura(nombre)
-                )
-            }
-
-            binding.progressBar.visibility = View.GONE
-            adapter.submitList(subjectItems)
+            adapter.updateData(subjectsWithTeachers)
         }
     }
 
